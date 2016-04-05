ade7109294d4e84e42720a78bef5a216a3e1a9f8
forrest <luosenlin@huoqiu.cn>
2016-04-05 17:47:34 +0800

    Build the net frame

--- app/src/main/java/com/lsl/graduation/Configs.java
+++ app/src/main/java/com/lsl/graduation/Configs.java
@@ -10,7 +10,7 @@ import java.io.File;
 public class Configs {
     public static File cacheFile;
     public static void Init(Context context){
-        cacheFile = new File(context.getCacheDir()+"huoqiu");
+        cacheFile = new File(context.getCacheDir()+"graduation");
 //        version = getPackageVersion(context);
 //        WebView view = new WebView(context);
 //        userAgent = view.getSettings().getUserAgentString();
@@ -19,7 +19,8 @@ public class Configs {
 //        PhoneManager.getInstance(context).addOnNetWorkChangeListener(
 //                ApnManager.getInstance(context));
 //        initImageLoader(context);
-    };
+    }
+
     /**连接超时*/
     public final static int CONNECTION_TIMEOUT = 15 * 1000;
     /** SO连接超时*/
